0f55f0d239fcb8fa09aa1951ea320eb04f72a8d2
ASherstobitov <aesherstobitov7@gmail.com>
2020-04-14 21:45:10 +0500

    To add the second circle

--- Polimorphizm/src/com/company/Main.java
+++ Polimorphizm/src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Picture picture = new Picture();
         Rectangle rectangle = new Rectangle(5, 5, 5, 5);
         Circle circle = new Circle(5, 5, 5);
+        Circle circle2 = new Circle(5, 5, 5);
         picture.add(rectangle);
         picture.add(circle);
         double d = picture.getSquare();
